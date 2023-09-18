2983a52ffcd9a9567121197e659c8d9a74be259e
Abhinay Yadav <56730424+abhinay01-rgb@users.noreply.github.com>
2023-09-18 14:56:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,48 +3,28 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
-from hugchat import hugchat
-from hugchat.login import Login
 
-# Log in to huggingface and grant authorization to huggingchat
-sign = Login(email, passwd)
-cookies = sign.login()
-
-# Save cookies to the local directory
-cookie_path_dir = "./cookies_snapshot"
-sign.saveCookiesToDir(cookie_path_dir)
-
-# Load cookies when you restart your program:
-# sign = login(email, None)
-# cookies = sign.loadCookiesFromDir(cookie_path_dir) # This will detect if the JSON file exists, return cookies if it does and raise an Exception if it's not.
-
-# Create a ChatBot
-chatbot = hugchat.ChatBot(cookies=cookies.get_dict())  # or cookie_path="usercookies/<email>.json"
-print(chatbot.chat("Hi!"))
-
-# Create a new conversation
-id = chatbot.new_conversation()
-chatbot.change_conversation(id)
-
-# Get conversation list
-conversation_list = chatbot.get_conversation_list()
-
-# Switch model (default: meta-llama/Llama-2-70b-chat-hf. )
-chatbot.switch_llm(0) # Switch to `OpenAssistant/oasst-sft-6-llama-30b-xor`
-chatbot.switch_llm(1) # Switch to `meta-llama/Llama-2-70b-chat-hf`
-st.set_page_config(page_title="Watchwise Chatbot")
+st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
 
 # Sidebar contents
 with st.sidebar:
-    st.title('🤗💬Watchwise')
+    st.title('🤗💬 HugChat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [HugChat](https://github.com/Soulter/hugging-chat-api)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
     
+    💡 Note: No API key required!
+    ''')
     add_vertical_space(5)
-    st.write('Made with ❤️')
+    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
 
 # Generate empty lists for generated and past.
 ## generated stores AI generated responses
 if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm Watchwise, How may I help you?"]
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
 ## past stores User's questions
 if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi!']
