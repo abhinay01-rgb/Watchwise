246d205200d9c5893c5cdce69939aece863ab640
Abhinay Yadav <56730424+abhinay01-rgb@users.noreply.github.com>
2023-09-18 14:46:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+st.set_page_config(page_title="Watchwise")
 
 # Sidebar contents
 with st.sidebar:
